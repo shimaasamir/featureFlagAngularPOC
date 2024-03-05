594dcfd93d26d91d1f98e4ad4f127ad82079fa19
Shimaa Samir <shimaa.samir@celfocus.com>
2024-03-05 21:29:24 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import {
+  RemoteConfig,
+  getRemoteConfig,
+  getValue,
+  fetchAndActivate,
+  getAll,
+  fetchConfig,
+} from '@angular/fire/remote-config';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'firebase-remote-config';
+export class AppComponent implements OnInit {
+  title = 'test';
+  dynamicText: any;
+  config: any;
+  // constructor(private remoteConfig: RemoteConfig) {}
+  private remoteConfig: RemoteConfig = inject(RemoteConfig);
+
+  ngOnInit() {
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: 'AIzaSyAPTJTsIjv82N_l6b1WVkMhaixag-gZMEU',
+      authDomain: 'test-fa1dd.firebaseapp.com',
+      projectId: 'test-fa1dd',
+      storageBucket: 'test-fa1dd.appspot.com',
+      messagingSenderId: '647447194109',
+      appId: '1:647447194109:web:4c729bbe175dd3f9abd194',
+      measurementId: 'G-EE8KPQD5P9',
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    // Initialize Remote Config and get a reference to the service
+    const remoteConfig = getRemoteConfig(app);
+    remoteConfig.settings.minimumFetchIntervalMillis = 3600000;
+
+    fetchAndActivate(remoteConfig)
+      .then(() => {
+        const allVal = getAll(remoteConfig);
+        console.log('all', allVal);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { RemoteConfig, RemoteConfigModule } from '@angular/fire/remote-config';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { RemoteConfigModule } from '@angular/fire/remote-config';
+// import { AngularFireRemoteConfigModule } from '@angular/fire/remote-config';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyAPTJTsIjv82N_l6b1WVkMhaixag-gZMEU',
+        authDomain: 'test-fa1dd.firebaseapp.com',
+        projectId: 'test-fa1dd',
+        storageBucket: 'test-fa1dd.appspot.com',
+        messagingSenderId: '647447194109',
+        appId: '1:647447194109:web:4c729bbe175dd3f9abd194',
+        measurementId: 'G-EE8KPQD5P9',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+    RemoteConfigModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
